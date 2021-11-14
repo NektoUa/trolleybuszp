709ee30d4ccdab2aca73a3e32b4ffd3f78a2af87
NektoUa <53370133+NektoUa@users.noreply.github.com>
2021-11-14 17:43:15 +0200

    little change

--- javascript/scriptMaps.js
+++ javascript/scriptMaps.js
@@ -1,12 +1,12 @@
 let a = [['3trol.jpg', 'Схема маршруту № 3'], ['14trol.jpg', 'Схема маршруту № 14']];
-let arrA = 0;
+let ZERO = 0;
 let k = 0;
 
 function makeMin() {
     a.forEach(elem => {
         let imag = document.createElement('img');
         imag.classList.add("img-12-min");
-        if (elem == arrA) imag.classList.add("active-img");
+        if (elem == ZERO) imag.classList.add("active-img");
         imag.src = './images/maps/' + elem[0];
         imag.alt = elem[1];
         document.querySelector('div-12-wrapper').appendChild(imag);
@@ -15,13 +15,13 @@ function makeMin() {
 }
 
 function removE(event) {
-    return document.querySelector('img[alt="' + a[arrA][1] + '"]').classList.remove('active-img');
+    return document.querySelector(`img[alt="${a[ZERO][1]}"]`).classList.remove('active-img');
 }
 
-function inHTML(arrA, event) {
-    document.querySelector('img[alt="' + a[arrA][1] + '"]').classList.add('active-img');
-    document.querySelector('.img-12-text').innerHTML = a[arrA][1];
-    let imge = './images/maps/' + a[arrA][0];
+function inHTML(ZERO, event) {
+    document.querySelector(`img[alt="${a[ZERO][1]}"]`).classList.add('active-img');
+    document.querySelector('.img-12-text').innerHTML = a[ZERO][1];
+    let imge = './images/maps/' + a[ZERO][0];
     document.querySelector('.img-12-max').attributes[0]['textContent'] = imge;
 }
 
@@ -32,40 +32,38 @@ function touchImg(event) {
     let wA = wrap.attributes[1].nodeValue
     for (let i in a) {
         if (wA == imge + a[i][0]) {
-            arrA = i;
+            ZERO = i;
         }
     }
-    inHTML(arrA);
+    inHTML(ZERO);
 }
 
 function nextImg(event) {
     removE();
-    arrA++;
-    if (arrA === (a.length)) {
-        arrA = 0;
+    ZERO++;
+    if (ZERO === (a.length)) {
+        ZERO = 0;
     }
-    inHTML(arrA);
+    inHTML(ZERO);
 }
 
 function prevImg(event) {
-    if (arrA == 0) {
+    if (ZERO == 0) {
         removE();
-        arrA = a.length - 1;
+        ZERO = a.length - 1;
     } else {
         removE();
-        arrA--;
+        ZERO--;
     }
-    inHTML(arrA);
+    inHTML(ZERO);
 }
 
 function resetImg() {
     removE();
-    arrA = 0;
-    document.querySelector('img[alt="' + a[arrA][1] + '"]').classList.add('active-img');
-    document.querySelector('.img-12-text').innerHTML = a[arrA][1];
+    ZERO = 0;
+    document.querySelector(`img[alt="${a[ZERO][1]}"]`).classList.add('active-img');
+    document.querySelector('.img-12-text').innerHTML = a[ZERO][1];
 }
 
 makeMin();
 document.querySelector('div-12-wrapper').addEventListener('click', touchImg);
-
-
